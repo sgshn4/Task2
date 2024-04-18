df894c1aa146a5eb5355e8b85e558150517e3eca
sgshn4 <davidov.oleg2018@yandex.ru>
2024-04-18 16:45:14 +0300

    Init commit

---
+++ app/src/main/java/com/pereslavtsev_o_yu/task2/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.pereslavtsev_o_yu.task2
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.FragmentManager
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.pereslavtsev_o_yu.task2.adapter.ViewPagerAdapter
+import com.pereslavtsev_o_yu.task2.ui.theme.Task2Theme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.tabs.TabLayoutMediator
+
+val animalsArray = arrayOf(
+    "Cat",
+    "Dog",
+    "Bird"
+)
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+        val viewPager = findViewById<ViewPager2>(R.id.pager)
+        val tabLayout = findViewById<TabLayout>(R.id.tab)
+        val fragmentManager = getFragmentManager()
+        val adapter = ViewPagerAdapter(, lifecycle)
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Task2Theme {
+        Greeting("Android")
+    }
+}
